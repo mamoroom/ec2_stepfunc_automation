1ca8572a783003ca9dd252124055bfb602fbc534
MamoruMurakami <mamoru@prty.jp>
2017-02-21 12:27:23 +0900

    update test script

--- script/s3_uploader.sh
+++ script/s3_uploader.sh
@@ -1,3 +1,3 @@
 #! /bin/bash 
-aws s3 cp $1 ${S3_RESULT_PATH}/${2}
+aws s3 cp $1 ${S3_RESULT_PATH}/${1}
 #aws s3 cp $2 ${S3_RESULT_PATH}/${2}
--- script/sample.py
+++ script/sample.py
@@ -6,16 +6,17 @@ from PIL import Image
 
 IMAGE_EXTENTION = '.jpg'
 INPUT_FILE_PATH = './data/input/sample' + IMAGE_EXTENTION
-OUTPUT_DIR_PATH = './data/output/'
 SCRIPT_PATH     = './s3_uploader.sh'
+
+args = sys.argv 
+i_width = args[1]
+i_height = args[2]
 
 now = datetime.datetime.now().strftime('%s')
 out_file_name = now+"_sample"+IMAGE_EXTENTION
-out_file_path = OUTPUT_DIR_PATH+out_file_name
-#out_file_path2 = OUTPUT_DIR_PATH+now+"_sample"+'.png'
-Image.open(INPUT_FILE_PATH).resize((1512,2016)).save(out_file_path)
+Image.open(INPUT_FILE_PATH).resize((i_width,i_height)).save(out_file_name)
 time.sleep(5)
-shell_result = os.system('sh ' + SCRIPT_PATH + ' ' + out_file_path + ' ' + out_file_name)
+shell_result = os.system('sh ' + SCRIPT_PATH + ' ' + out_file_name + ' ' + out_file_name)
 if (shell_result) :
     print 'ERROR'
 else if :
